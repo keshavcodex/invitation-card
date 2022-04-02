6c89c815cf6285f7e186206618ee6166e8347082
keshavcodex1 <keshavcodex@gmail.com>
2022-04-02 08:19:00 +0530

    updated date

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -9,7 +9,7 @@ function Entry(props) {
                     <h1 className="u-text-palette-1-base u-text-1">Birthday&nbsp;Invitation For {props.name}
                     </h1>
                     <p className="u-text u-text-2">With god’s blessing and your love we are throwing 20th birthday party on this
-                        <b> <u>10th April</u>&nbsp;</b>and I would like you to double my happiness with your
+                        <b> <u>4th April</u>&nbsp;</b>and I would like you to double my happiness with your
                         presence.&nbsp;Awaiting you! Lots of love from <strong>Khushi, Alka & Keshav.</strong>
                     </p>
                     <a href="#" className="u-black u-btn u-button-style u-btn-1">Venue: Notified soon</a>
